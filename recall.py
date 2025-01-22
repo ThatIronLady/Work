252fc0d10cf2578eb6d5d237e829bdd0b8d85bdf
ThatIronLady <albiejt@icloud.com>
2025-01-22 10:29:13 +0000

    Columnar Reposition Cipher

--- recall.py
+++ recall.py
@@ -1,12 +1,34 @@
-colour= input()
-print(colour)
+import random
 
-input_1=input()
-input_2=input()
-input_3=input()
-input_4=input()
-input_5=input()
+class column:
+  def __init__(self,order,line):
+    self.__order=order
+    self.line=line
 
-print(input_5,input_4,input_3,input_2,input_1)
+def main():
+  arr=[]
+  plaintext=input("Enter message to be encrypted: ")
+  size=input("Enter encryption factor: ")
+  b=True
+  newarr=[]
+  rando_store=[]
+  while b!=True:
+    a=plaintext[:size]
+    plaintext=plaintext[size:]
+    rando_check=False
+    rando=random.randint(1,size)
+    while rando in rando_store:
+        rando=random.randint(1,size)
+    rando_store.append(rando)
+    for i in range(len(a):
+      newerarr.append(a[:i:])
+    newarr.append(column(rando,newerarr))
+    if plaintext=='':
+      b=False
+  for i in range(len(newarr)):
+    for j in range(len(i.line)):
+      print(i.line[j])
 
-
+    
+    
+    
